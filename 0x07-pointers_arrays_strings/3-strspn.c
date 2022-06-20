45dcd6c60fc03cc86d79bddd3d7817c60a00061f
Nath <byamnat@gmail.com>
2022-06-20 05:15:23 -0700

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,22 +1,32 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * _strspn - return length of string that matches values consistently
+ * @s: string to search
+ * @accept: target matches
+ * Return: number of bytes consecutively matched
+ */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int c = 0;
-	char *t = accept;
+	int i = 0, j;
+	int matches = 0;
 
-	while (*s++)
+	while (s[i] != '\0') /*iterate through string*/
 	{
-		while (*accept++)
-			if (*(s - 1) == *(accept - 1))
+
+		for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
+		{
+			if (s[i] == accept[j]) /*record & break at first match*/
 			{
-				c++;
+				matches++;
 				break;
 			}
-		if (!(*--accept))
-			break;
-		accept = t;
+			if (accept[j + 1] == '\0' && s[i] != accept[j])
+				return (matches);/*return if idx doesn't match*/
+		}
+		i++;
 	}
-	return (c);
+	return (matches); /* return num if all match till end */
+
 }
